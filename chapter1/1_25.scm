003212d31cf7ba5c1fc792d70d3631e41c512e7d
LucasNomi <lucas04.dev@gmail.com>
2024-02-15 21:35:56 -0300

    feat: alternative expmod using already existing fast-expt algorithm

---
+++ chapter1/1_25.scm
@@ -0,0 +1,11 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
